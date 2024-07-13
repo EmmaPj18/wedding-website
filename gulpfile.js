c2e276ef8fa8ad1859961029a881dd938997ecf5
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2024-07-13 17:48:49 -0400

    Add gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sync = require("browser-sync").create();
 
 // compile scss to css
 gulp.task('sass', function () {
@@ -28,3 +29,43 @@ gulp.task('minify-js', function () {
 
 // default task
 gulp.task('default', gulp.series('sass', 'minify-js'));
+
+gulp.task('serve', function () {
+    sync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch('./sass/**/*.scss', sassCompileWatch);
+    gulp.watch('./js/scripts.js', minifyJsWatch);
+    gulp.watch("./public/**.html").on('change', sync.reload);
+});
+
+function minifyJs() 
+{
+    return gulp.src('./js/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({basename: 'scripts.min'}))
+        .pipe(gulp.dest('./js'));
+}
+
+function minifyJsWatch() 
+{
+    return minifyJs()
+    .pipe(sync.stream());
+}
+
+function sassCompile() 
+{
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({basename: 'styles.min'}))
+        .pipe(gulp.dest('./css'));
+}
+
+function sassCompileWatch() 
+{
+    return sassCompile()
+    .pipe(sync.stream());
+}
